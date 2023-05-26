ebc7aede3197491214d83a607e04ba96f40d70e9
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-05-26 20:43:54 +0100

    working

--- src/BasicUploadRequest.php
+++ src/BasicUploadRequest.php
@@ -2,14 +2,20 @@
 
 namespace Nelwhix\VetacloudLaravel;
 
+
+use Saloon\Contracts\Body\BodyRepository;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Data\MultipartValue;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Traits\Body\HasMultipartBody;
 
-class BasicUploadRequest extends Request
+class BasicUploadRequest extends Request implements HasBody
 {
+    use HasMultipartBody;
     protected Method $method = Method::POST;
 
-    public function __construct(private string $routeName) {}
+    public function __construct(private string $routeName, private $file, private string $path) {}
 
     protected function defaultHeaders(): array {
         return [
